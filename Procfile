1638bc8e718905e3c8fb64ab7bd284cbff7b51b7
Krishna Sai Vandavasi <kvandavasi@internal.salesforce.com>
2022-03-10 08:07:06 +0530

    third commit

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: java -jar target/java-getting-started-1.0.jar
 web: java -jar target/java-getting-started-1.0.jar
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -56,6 +56,8 @@ public class Main {
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
+
+      System.out.println(""+ dataSource.toString());
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
       stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
@@ -81,7 +83,6 @@ public class Main {
     } else {
       HikariConfig config = new HikariConfig();
       config.setJdbcUrl(dbUrl);
-      System.out.println(config.getDriverClassName()+"");
       return new HikariDataSource(config);
     }
   }
