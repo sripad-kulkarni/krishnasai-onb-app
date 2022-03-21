9ac5a7ec2ef2475ef8b196d015a425865dc9e03c
Krishna Sai Vandavasi <kvandavasi@internal.salesforce.com>
2022-03-21 07:08:52 +0530

    Man env variable

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -87,7 +87,8 @@ public class Main {
   @RequestMapping("/hello")
   @ResponseBody
   String index3() {
-    return "Hello";
+    String name="Man";
+    return "Hello"+System.getenv(name);
   }  
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
