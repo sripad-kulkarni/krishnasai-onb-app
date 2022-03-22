0d40f7abfd560fcbb03385c33592cc789acf3c8e
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2022-03-22 06:22:30 +0530

    Update MainTest.java

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-#public class MainTest {
-#     @Test
-#     public void evaluatesExpression() {
-#        int sum = 1+2+3+4;
-#        assertEquals(10, sum);
-#     }
-#}
+public class MainTest {
+     @Test
+     public void evaluatesExpression() {
+        int sum = 1+2+3+4;
+        assertEquals(10, sum);
+     }
+}
