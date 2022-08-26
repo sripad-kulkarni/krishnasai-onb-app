aaba331ad04a7521f8f4e8b28bababc773554f08
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2022-08-26 08:43:32 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -97,11 +97,11 @@ public class Main {
     try (Connection connection = dataSource.getConnection()) {
 
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticksss");
+      #stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      #stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-      ArrayList<String> output = new ArrayList<String>();
+      ArrayList<Character> output = new ArrayList<>();
       while (rs.next()) {
         output.add("Read from DB: " + rs.getTimestamp("tick"));
       }
